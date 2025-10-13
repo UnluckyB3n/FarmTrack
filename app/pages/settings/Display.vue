753d5f4340befd32f0107bfa2c15845a8e5109e4
Benjamin Ayensu <Benjamin12345626@gmail.com>
2025-10-13 13:44:04 -0400

    feat: add Switch component and related files
    feat: implement Table component and its subcomponents
    
    feat: create Tabs component and its subcomponents
    
    feat: add TagsInput component and related files
    
    feat: implement Textarea component
    
    feat: add ToggleGroup and Toggle components
    
    feat: update Settings layout and pages for account management

---
+++ app/pages/settings/Display.vue
@@ -0,0 +1,10 @@
+<template>
+  <SettingsLayout>
+    <DisplayForm />
+  </SettingsLayout>
+</template>
+
+<script setup lang="ts">
+import DisplayForm from '~/components/Settings/DisplayForm.vue';
+import SettingsLayout from '~/layouts/SettingsLayout.vue';
+</script>
